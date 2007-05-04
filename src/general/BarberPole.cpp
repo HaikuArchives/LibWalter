64727abdbb93bcd8969ffbe2b9521018c72dba0e
darkwyrm <darkwyrm@4c7b89fb-9b2e-0410-b0fa-81d8202d9b54>
2007-05-04 23:12:25 +0000

    Style cleanup Removed B prefix from class name -- we'll add a different one later Turned the #defined shade count into a hardcoded number -- the #define wasn't necessary or pretty Initialized fHighColors and fLowColors to a nice-looking blue and white -- previously set to random numbers when instantiated Non-3D look now uses assigned high and low colors.
    git-svn-id: http://svn.osdrawer.net/libwalter@24 4c7b89fb-9b2e-0410-b0fa-81d8202d9b54

--- src/general/BarberPole.cpp
+++ src/general/BarberPole.cpp
@@ -7,9 +7,12 @@
 #include "BarberPole.h"
 #include <Region.h>
 
-BBarberPole::BBarberPole(BRect pRect, const char *pName, uint32 resizingMode, uint32 flags, int pDirection)
-		: BView(pRect, pName, resizingMode, flags | B_WILL_DRAW | B_PULSE_NEEDED) {
-		
+BarberPole::BarberPole(BRect frame, const char *name, uint32 resizingMode, uint32 flags,
+						int pDirection)
+ :	BView(frame, name, resizingMode, flags | B_WILL_DRAW | B_PULSE_NEEDED),
+	fDirection(pDirection),
+ 	fStyle(BARBERPOLE_FANCY)
+{
 	SetViewColor(B_TRANSPARENT_COLOR);
 	fStripes.data[0] = 0x0f;
 	fStripes.data[1] = 0x1e;
@@ -18,65 +21,95 @@ BBarberPole::BBarberPole(BRect pRect, const char *pName, uint32 resizingMode, ui
 	fStripes.data[4] = 0xf0;
 	fStripes.data[5] = 0xe1;
 	fStripes.data[6] = 0xc3;
-	fStripes.data[7] = 0x87;	
-	fDirection = pDirection;
+	fStripes.data[7] = 0x87;
+	
+	const rgb_color highColor = { 0, 0, 255, 255 };
+	SetHighColor(highColor);
+	const rgb_color lowColor = { 255, 255, 255, 255 };
+	SetLowColor(lowColor);
 };
 
 
-#if BB_LOOK_3D
-void BBarberPole::SetColors(rgb_color* colors, rgb_color c) {
-	int alpha = 255/BB_NUM_SHADES;
-	for (int i = 0; i < BB_NUM_SHADES; i ++) {
-		colors[i].red = c.red * alpha / 255;
-		colors[i].green = c.green * alpha / 255;
-		colors[i].blue = c.blue * alpha / 255;
-		colors[i].alpha = 255;
-		alpha += 255/BB_NUM_SHADES;
-	}
-}
-void BBarberPole::SetLowColor(rgb_color c) {
+void
+BarberPole::SetLowColor(const rgb_color &c)
+{
 	SetColors(fHighColors, c);
 }
 
-void BBarberPole::SetHighColor(rgb_color c) {
+
+void
+BarberPole::SetHighColor(const rgb_color &c)
+{
 	SetColors(fLowColors, c);
 }
-#endif
 
-void BBarberPole::Draw(BRect r) {
-#if BB_LOOK_3D
-	BRect b(Bounds());
-	BRect b1;
-	for (int i = 0; i < BB_NUM_SHADES; i ++) {
-		BRegion r(b);
-		if (i != (BB_NUM_SHADES-1)) {
-			b1 = b.InsetByCopy(1, 1);
-			r.Exclude(b1);
+
+void
+BarberPole::Draw(BRect r)
+{
+	if (fStyle == BARBERPOLE_FANCY) {	
+		BRect b(Bounds());
+		BRect b1;
+		for (int i = 0; i < 4; i ++) {
+			BRegion r(b);
+			if (i != (4-1)) {
+				b1 = b.InsetByCopy(1, 1);
+				r.Exclude(b1);
+			}
+			ConstrainClippingRegion(&r);
+			BView::SetHighColor(fHighColors[i]); BView::SetLowColor(fLowColors[i]);
+			FillRect(b, fStripes);
+			b.InsetBy(1, 1);
 		}
-		ConstrainClippingRegion(&r);
-		BView::SetHighColor(fHighColors[i]); BView::SetLowColor(fLowColors[i]);
-		FillRect(b, fStripes);
-		b.InsetBy(1, 1);
+	} else {
+		BView::SetHighColor(fHighColors[4 - 1]);
+		BView::SetLowColor(fLowColors[4 - 1]);
+		FillRect(r, fStripes);
 	}
-#else
-	FillRect(r, fStripes);
-#endif
 }
 
-void BBarberPole::Pulse() {
-	if(fDirection == FROM_RIGHT_TO_LEFT){
+
+void
+BarberPole::Pulse(void)
+{
+	if (fDirection == BARBERPOLE_FROM_RIGHT) {
 		uchar tmp = fStripes.data[0];
-		for (int j = 0; j < 7; ++j) {
+		for (int j = 0; j < 7; ++j)
   			fStripes.data[j] = fStripes.data[j+1];
-		}
 		fStripes.data[7] = tmp;
 	} else {
 		uchar tmp = fStripes.data[7];
-		for (int j = 7; j > 0; --j) {
+		for (int j = 7; j > 0; --j)
   			fStripes.data[j] = fStripes.data[j-1];
-		}
 		fStripes.data[0] = tmp;
 	}
 	Invalidate();
 }
 
+
+void
+BarberPole::SetStyle(int style)
+{
+	fStyle = style;
+}
+
+
+int
+BarberPole::Style(void) const
+{
+	return fStyle;
+}
+
+
+void
+BarberPole::SetColors(rgb_color *colors, const rgb_color &c)
+{
+	int alpha = 255 / 4;
+	for (int i = 0; i < 4; i++) {
+		colors[i].red = c.red * alpha / 255;
+		colors[i].green = c.green * alpha / 255;
+		colors[i].blue = c.blue * alpha / 255;
+		colors[i].alpha = 255;
+		alpha += 255 / 4;
+	}
+}
--- src/general/BarberPole.h
+++ src/general/BarberPole.h
@@ -4,37 +4,46 @@
  * All rights reserved. Distributed under the terms of the MIT License.
  */
 
-#ifndef _BARBERPOLE_H_
-#define _BARBERPOLE_H_
+#ifndef BARBERPOLE_H_
+#define BARBERPOLE_H_
 
 #include <View.h>
 
-const int FROM_RIGHT_TO_LEFT = 0;
-const int FROM_LEFT_TO_RIGHT = 1;
+enum {
+	BARBERPOLE_FROM_RIGHT = 0,
+	BARBERPOLE_FROM_LEFT
+};
 
-#define BB_LOOK_3D 1
-#define BB_NUM_SHADES 4
+enum {
+	BARBERPOLE_SIMPLE = 0,
+	BARBERPOLE_FANCY
+};
 
-class BBarberPole : public BView {
+class BarberPole : public BView
+{
 public:
-	BBarberPole(BRect pRect, const char *pName, uint32 resizingMode, uint32 flags, int pDirection = FROM_LEFT_TO_RIGHT);
+					BarberPole(BRect frame, const char *name, uint32 resizingMode,
+								uint32 flags, int pDirection = BARBERPOLE_FROM_LEFT);
 
-#if BB_LOOK_3D
-	void SetLowColor(rgb_color c);
-	void SetHighColor(rgb_color c);
-#endif
-	void Draw(BRect rect);
-	void Pulse();
+			void	SetLowColor(const rgb_color &c);
+			void	SetHighColor(const rgb_color &c);
+	
+	virtual void	Draw(BRect rect);
+	virtual void	Pulse();
+	
+			void	SetStyle(int style);
+			int		Style(void) const;
 	
 private:
-	static void SetColors(rgb_color* colors, rgb_color c);
+	static	void	SetColors(rgb_color* colors, const rgb_color &c);
 	
-	int		fDirection;
-	pattern fStripes;
-#if BB_LOOK_3D
-	rgb_color fHighColors[BB_NUM_SHADES];
-	rgb_color fLowColors[BB_NUM_SHADES];
-#endif
+	int				fDirection;
+	pattern			fStripes;
+	
+	rgb_color		fHighColors[4];
+	rgb_color		fLowColors[4];
+	
+	int				fStyle;
 };
 
 #endif
