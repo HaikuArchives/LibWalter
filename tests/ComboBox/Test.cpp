7573c39dc58d07d7962f6ebb3c2787c317b54f1a
urnenfeld <urnenfeld@4c7b89fb-9b2e-0410-b0fa-81d8202d9b54>
2008-01-20 15:33:47 +0000

    Test app code was missing in previous commit
    git-svn-id: http://svn.osdrawer.net/libwalter@79 4c7b89fb-9b2e-0410-b0fa-81d8202d9b54

--- tests/ComboBox/Test.cpp
+++ tests/ComboBox/Test.cpp
@@ -9,64 +9,25 @@
 #include <Resources.h>
 #include <Window.h>
 
-#include "ComboBox.h"
-
-#define TEST_MESSAGE 'test'
-
-class MyWindow : public BWindow { // --------------------------------------------------------------
-private:
-	ComboBox* combo;
-public:
-
-MyWindow(void) :
-	BWindow(BRect(100, 100, 400, 400), "ComboBox test", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS)
-{
-	BView *back = new BView(Bounds(), NULL, B_FOLLOW_ALL_SIDES, 0);
-	AddChild(back);
-	
-	combo = new ComboBox(BRect(10,10,260,260), "Combo test", 
-	                                           "Combo test label", 
-	                                           "initial text", new BMessage(TEST_MESSAGE));
-
-	combo->AddItem("item number 1");
-	combo->AddItem("item number 2");
-	combo->AddItem("item number 3");
-	combo->AddItem("item number 4");			
-	back->AddChild(combo);
-
-}
-
-
-void MessageReceived(BMessage *message)
-{
-	if (message->what == TEST_MESSAGE)
-		(new BAlert("Hello!", "Hello!", "OK"))->Go();
-	BWindow::MessageReceived(message);
-}
-
-bool QuitRequested(void)
-{
-	be_app->PostMessage(B_QUIT_REQUESTED);
-	return BWindow::QuitRequested();
-}
-
-}; // MyWindow ------------------------------------------------------------------------------------
-
+#include "InputRequest.h"
 
 class MyApp : public BApplication { // ------------------------------------------------------------
-private:
-	MyWindow *fWindow;
 public:
 
-MyApp(void) : BApplication("application/libwalter-ComboBox-test")
+MyApp(void) : BApplication("application/libwalter-InputRequest")
 {
-	fWindow = new MyWindow();
+
+	char reply[256];
+	
+	InputRequest* ir = new InputRequest("This is the title", "This the label", 
+	                                    "Here comes the text", "button label");
+	                                    
+	ir->Go((char**)&reply);
+	
+	be_app->PostMessage(B_QUIT_REQUESTED);
+
 }
 
-void ReadyToRun(void)
-{
-	fWindow->Show();
-}
 
 }; // MyApp ---------------------------------------------------------------------------------------
 
